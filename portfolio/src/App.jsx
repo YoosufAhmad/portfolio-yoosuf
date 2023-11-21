051ddec2a1a29224019bbb463f97cc05a8ff4b07
yoosuf <yoosuf@Yoosuf>
2023-11-21 00:16:47 +0000

    positioning adjustments and more info added to projects card

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -24,21 +24,26 @@ export default function App() {
               Yoosuf Ahmad
             </h2>
             <h3 className="text-2xl py-2">Front-End Developer</h3>
-            <p className="text-md py-5 leading-8 text-slate-600">
+            <p className="text-md py-5 leading-8 max-w-3xl mx-auto">
               Welcome to my little corner of the Internet. Here, I share and
               showcase my tinkering on the web. I am passionate about intuitive
               design and love to craft beautiful premium websites for anyone who
               is ready to stand out from the crowd.
             </p>
-            <h3 className="text-3xl py-2 text-amber-950 font-medium mt-4">Projects</h3>
+            <h3 className="text-3xl py-2 text-amber-950 font-medium mt-4">
+              Projects
+            </h3>
           </div>
 
-          <div className="flex flex-col space-y-6 pb-4">
-
+          <div className="space-y-6 pb-4 ">
             <div class="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
               <div class="md:flex relative">
                 <div class="md:shrink-0 overflow-hidden">
-                  <img src={projectpic1} alt="coffee picture by farza mohamadi " className="object-cover md:max-w-xs hover:scale-110 transition duration-500 cursor-pointer object-cover"/>
+                  <img
+                    src={projectpic1}
+                    alt="coffee picture by farza mohamadi "
+                    className="object-cover md:max-w-xs hover:scale-110 transition duration-500 cursor-pointer object-cover"
+                  />
                 </div>
                 <div class="p-8">
                   <div class="uppercase tracking-wide text-sm text-amber-800 font-semibold">
@@ -57,27 +62,32 @@ export default function App() {
                     coffee in the morning, allowing you to scroll till you
                     finish your coffee.
                   </p>
+                  <p className="pt-4 text-amber-950 font-medium">React - JavaScript - Axios</p>
+
                   <a
-                  href="https://coffee-news-nc.netlify.app"
-                  className="block text-1xl my-2 px-4 py-2 bg-amber-950 text-white rounded-md md:hover:bg-amber-900"
-                >
-                  Live Demo
-                </a>
-                <a
-                  href="#"
-                  className="block text-1xl my-2 px-4 py-2 bg-amber-950 text-white rounded-md md:hover:bg-amber-900"
-                >
-                  Code
-                </a>
+                    href="https://coffee-news-nc.netlify.app"
+                    className="block text-1xl text-center mt-8 px-4 py-2 bg-amber-950 text-white rounded-md md:hover:bg-amber-900"
+                  >
+                    Live Demo
+                  </a>
+                  <a
+                    href="#"
+                    className="block text-1xl text-center my-2 px-4 py-2 bg-amber-950 text-white rounded-md md:hover:bg-amber-900"
+                  >
+                    Code
+                  </a>
                 </div>
-                
               </div>
             </div>
 
             <div class="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
               <div class="md:flex relative">
                 <div class="md:shrink-0 overflow-hidden">
-                  <img src={projectpic2} alt="coffee and newspaper picture by celine ylmz" className="object-cover md:max-w-xs hover:scale-110 transition duration-500 cursor-pointer object-cover" />
+                  <img
+                    src={projectpic2}
+                    alt="coffee and newspaper picture by celine ylmz"
+                    className="object-cover md:max-w-xs hover:scale-110 transition duration-500 cursor-pointer object-cover"
+                  />
                 </div>
                 <div class="p-8">
                   <div class="uppercase tracking-wide text-sm text-amber-800 font-semibold">
@@ -96,18 +106,19 @@ export default function App() {
                     coffee in the morning, allowing you to scroll till you
                     finish your coffee.
                   </p>
+                  <p className="pt-4 text-amber-950 font-medium">PostgreSQL - Express.js - JavaScript</p>
                   <a
-                  href="https://coffeenews.onrender.com/api/"
-                  className="block text-1xl my-2 px-4 py-2 bg-amber-950 text-white rounded-md md:hover:bg-amber-900"
-                >
-                  Live Demo
-                </a>
-                <a
-                  href="#"
-                  className="block text-1xl my-2 px-4 py-2 bg-amber-950 text-white rounded-md md:hover:bg-amber-900"
-                >
-                  Code
-                </a>
+                    href="https://coffeenews.onrender.com/api/"
+                    className="block text-1xl text-center mt-8 px-4 py-2 bg-amber-950 text-white rounded-md md:hover:bg-amber-900"
+                  >
+                    Live Demo
+                  </a>
+                  <a
+                    href="#"
+                    className="block text-1xl text-center mt-2 px-4 py-2 bg-amber-950 text-white rounded-md md:hover:bg-amber-900"
+                  >
+                    Code
+                  </a>
                 </div>
               </div>
             </div>
